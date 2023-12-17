8d0a381024afe5d420bb8967b82c0b87e8bac41e
fr33zingH3ll <samuelnicod66@gmail.com>
2023-12-17 11:42:09 +0100

    fix: modification du install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ cd game-engine/
 npm install
 
 # Installation pour le front end
-cd front-end/
+cd ../front-end/
 npm install
 
 # Installation pour le back end
-cd back-end/
+cd ../back-end/
 npm install
