aa981ea163ccf3eb6a1e1dee801b3d684595b999
fr33zingH3ll <samuelnicod66@gmail.com>
2024-05-23 20:04:22 +0200

    feat: quasiemnt rien a changé

--- front-end/main.js
+++ front-end/main.js
@@ -5,7 +5,8 @@ import { MainGame } from "./src/scenes/MainGame.js";
 const token = localStorage.getItem('token');
 if (token && token != "undefined") {
     (async () => {
-        const socket = new Socket('ws://galactic-seeker-api.freezinghell.net/');
+        const socket = new Socket('wss://galactic-seeker-api.freezinghell.net/');
+        // const socket = new Socket('ws://localhost:3000/');
         await socket.init();
         await socket.sendHandshake(token);
         const game = new MainGame(socket);
